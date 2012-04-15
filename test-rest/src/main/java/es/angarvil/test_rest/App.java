69637bb85391ee00a4c545aaf2d3c3c2ae5756b2
Angarvil <angarvil@gmail.com>
2012-04-15 14:54:10 +0200

    First commit

---
+++ test-rest/src/main/java/es/angarvil/test_rest/App.java
@@ -0,0 +1,13 @@
+package es.angarvil.test_rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
